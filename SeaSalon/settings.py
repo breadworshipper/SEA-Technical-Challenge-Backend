f9abb82a93db3dc7780c305f4c303eeac820e4b4
breadworshipper <azhrayashna@gmail.com>
2024-06-25 13:25:39 +0700

    CHORE: Setup connection to database

--- SeaSalon/settings.py
+++ SeaSalon/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +78,12 @@ WSGI_APPLICATION = "SeaSalon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres.nbjaaqknnkbdfvkinsla',
+        'PORT': '5432',
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
     }
 }
 
