078ec513b9a25d131ac71df5d474b3014dbbfa6a
breadworshipper <azhrayashna@gmail.com>
2024-06-25 13:48:51 +0700

    FEAT: Added Reservation model

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    type_of_service = models.CharField(max_length=100, choices=[
+        ('Haircut', 'haircut'),
+        ('Manicure', 'manicure'),
+        ('Facial', 'facial'),
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+    date = models.DateField()
+    time = models.TimeField()
+    
