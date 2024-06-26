1117c24ce100f09ba39541861fb411a4597b4121
breadworshipper <azhrayashna@gmail.com>
2024-06-26 22:10:03 +0700

    FEAT: Review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=100)
+    review = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(choices=[1, 2, 3, 4, 5])
+    
